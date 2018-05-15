a19c977e6ef6fd4374707a10cf5afb5f4dec3bb4
marc antoine de morel <marcantoine.demorel@gmail.com>
2018-05-15 17:03:27 +0200

    done backend on task-manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,8 +7,36 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+  # New task
+  def new
+    @task = Task.new
+  end
   # Create one task
-  # Edit a task
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+  # Edit (get)
+  def edit
+    @task = Task.find(params[:id])
+  end
+  # Update a task (post)
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
   # Remove a task
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
 
   # Read all tasks
   get "/tasks", to: "tasks#index"
+  # Create one task
+  get "/tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
   # Display one task
   get "/tasks/:id", to: "tasks#show", as: "task"
-
+  # Edit one task
+  get "/tasks/:id/edit", to: "tasks#edit", as: "task_edit"
+  patch "tasks/:id", to: "tasks#update"
+  # Delete a task
+  delete "/tasks/:id", to: "tasks#destroy"
 end
